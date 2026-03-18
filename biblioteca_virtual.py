b4aae33ffa8eb0c08a02d98b176da0812bd61f9a
@Klisman_12 <138720438+EmanuelKlisman@users.noreply.github.com>
2026-03-18 10:07:28 -0300

    Ajuste das estruturas

--- biblioteca_virtual.py
+++ biblioteca_virtual.py
@@ -1,151 +1,147 @@
-### Inicializacao das listas vazias e declaração de variaveis ###
+### Inicializacao das listas vazias ###
 
 estante1 = []
 estante2 = []
 
 # 1.Estruturas dos menus de opções das estantes e interacao com o usuario
 
-nome = input(("Insira seu nome: "))
-print(F"Olá {nome}, seja bem vindo a sua biblioteca virtual! A seguir está o menu principal:" )
+usuario = input(("Insira seu nome: "))
+print(F"Olá {usuario}, seja bem vindo a sua biblioteca virtual! A seguir está o menu principal:" )
 
-# 2. Estrutura do Menu Principal
+# 2. Loop Estrutura do Menu Principal
+
 while True:   
-    menu_principal = input("- Opção 1 - Estante 1 \n- Opção 2 - Estante 2\n- Opção 3 - Sair do programa\nAguardando sua escolha....")
+    menu_principal = input(\
+                        "- Opção 1 - Estante 1 \n"
+                        "- Opção 2 - Estante 2\n"
+                        "- Opção 3 - Sair do programa\n"
+                        "  Aguardando sua escolha....")
 
  
-# 3. Estrutura das condições caso seja a estante1  / Acesso da estante 1
-    while  menu_principal == "1":
-                print("Acessando o menu da estante 1.....")
-                menu_estante1 = input("Opção 1 - Adicionar livros \nOpção 2 - Ver a quantidade de livros \nOpção 3 - Voltar ao Menu Principal\nAguardando sua escolha....")
-                if menu_estante1== "1":
-                    adicionar_livros = input("Digite o livro que você deseja adicionar: ")
-                    estante1.append(adicionar_livros)
-                    print(f"livro {adicionar_livros} adicionado com sucesso!")
-                    while True:
-                        continuar_ad = input("Deseja continua adicionando ou sair?S/N")
-                        if continuar_ad == "S": 
+# 4. Estrutura das condições caso seja a estante1  
+
+    if menu_principal == "1":
+                while True:
+                    print(f"--------Acessando o menu da estante {menu_principal}--------\n")
+                    menu_estante1 = input(    
+                                            "1 - Adicionar livros\n"
+                                            "2 - Ver quantidade\n"
+                                            "3 - Livros adicionados \n"
+                                            "Aguardando sua escolha...."
+                                        )
+                    
+# 4.1 - Estrutura de acesso a estante 1 / Adiciona livros
+
+                    if menu_estante1 == "1":
+                            while True:
                                 adicionar_livros = input("Digite o livro que você deseja adicionar: ")
                                 estante1.append(adicionar_livros)
-                                print(f"livro {adicionar_livros}adicionado com sucesso!")
-                                break
+                                print(f"livro {adicionar_livros} adicionado com sucesso!")
+                        
+                                continuar = input("Deseja continuar? S/N")
 
-                        else:
-                            print("Voltando para o menu da estante....")
-                            break
-                                        
+                                if continuar != "S": # Converte a letra para minuscula 
+                                 print("--------Voltando ao menu da estante--------")
+                                 break
+                                 
+# 4.2 - Estrutura de acesso a estante 1 / Quantidade de livros
 
-                elif menu_estante1 == "2":
-                                print(f"Sua estante possui {len(estante1)} livros")    
-                                while True:
-                                    adicionar_ou_nao = input("Deseja adicionar algum livro ou nao ?S/N")   
-                                    if adicionar_ou_nao == "S":
-                                        adicionar_livros = input("Digite o livro que você deseja adicionar: ")
-                                        estante1.append(adicionar_livros)  # append adciona no último indice / Final da fila
-                                        print("livro adicionado com sucesso!")
-                                    else:
-                                        while True: 
-                                            deseja_continuar2 = input("Escolha a opcao: \nOpcao 1 - Menu da Estante \nOpcao 2 - Menu Principal\nAguardando sua escolha....")
+                    elif menu_estante1 == "2":
+                            print(f"Sua estante possui {len(estante1)} livros ")
+                            while True:
+                                adicionar_livros = input("Deseja adicionar algum livro? S/N")
 
-                                            if deseja_continuar2 == "1":
-                                                print("Acessando o menu da estante .....")
-                                                menu_estante1 = input("Opção 1 - Adicionar livros \n Opção 2 - Ver a quantidade de livros \n Opção 3 - Voltar ao Menu Principal\nAguardando sua escolha....")
-                                                break
+                                if adicionar_livros == "S":
+                                    adicionar_livros = input("Digite o livro que você deseja adicionar: ")
+                                    estante1.append(adicionar_livros)
+                                    print(f"livro {adicionar_livros} adicionado com sucesso!")
+                                else:
+                                    print("--------Voltando ao menu da estante--------")
+                                    break
 
-                                            elif deseja_continuar2 == "2":
-                                                print("Retornando ao menu principal...")
-                                                menu_principal = input("- Opção 1 - Estante 1 \n- Opção 2 - Estante 2\n- Opção 3 - Sair do programa\nAguardando sua escolha....")
+# 4.3 - Estrutura de acesso a estante 1 / Livros Adicionados
 
-                                       
-                else:
-                    print("Retornando ao menu principal....")
-                    menu_principal = input("- Opção 1 - Estante 1 \n- Opção 2 - Estante 2\n- Opção 3 - Sair do programa\nAguardando sua escolha....")
-    
-                
-# 4. Estrutura das condições caso seja a estante2 / Acesso a estante 2
+                    elif menu_estante1 == "3":
+                            print(f"Seus livros adicionados são:{estante1}")
+                            input("Digite qualquer tecla para continuar")
+                            print("--------Voltando ao menu da estante--------")
+                            print(f"--------Acessando o menu da estante {menu_principal}--------\n")
+                            menu_estante1 = input(    
+                                            "1 - Adicionar livros\n"
+                                            "2 - Ver quantidade\n"
+                                            "3 - Livros adicionados \n"
+                                            "Aguardando sua escolha...."                   
+                                )
+                            break             
+   
 
-    while  menu_principal == "2":
-                    print("Acessando o menu da estante 2.....")
-                    menu_estante2 = input("Opção 1 - Adicionar livros \nOpção 2 - Ver a quantidade de livros \nOpção 3 - Voltar ao Menu Principal\nAguardando sua escolha....")
-                    if menu_estante2== "1":
+# 5. Estrutura das condições caso seja a estante2
+
+    elif menu_principal == "2":
+        while True:
+                    print(f"--------Acessando o menu da estante {menu_principal}--------\n")
+                    menu_estante2 = input(    
+                                            "1 - Adicionar livros\n"
+                                            "2 - Ver quantidade\n"
+                                            "3 - Livros adicionados \n"
+                                            "4 - Voltar ao menu principal\n"
+                                            "Aguardando sua escolha...."
+                                        )
+                    
+# 6.1 - Estrutura de acesso a estante 2 / Adiciona livros
+
+                    if menu_estante2 == "1":
+                            while True:
                                 adicionar_livros = input("Digite o livro que você deseja adicionar: ")
                                 estante2.append(adicionar_livros)
                                 print(f"livro {adicionar_livros} adicionado com sucesso!")
-                                while True:
-                                    continuar_ad = input("Deseja continua adicionando ou sair?S/N")
-                                    if continuar_ad == "S": 
-                                        adicionar_livros = input("Digite o livro que você deseja adicionar: ")
-                                        estante1.append(adicionar_livros) # append adciona no último indice / Final da fila
-                                        print(f"livro {adicionar_livros}adicionado com sucesso!")
-                                    else:
-                                        print("Voltando para o menu da estante....")
-                                        break
+                        
+                                continuar = input("Deseja continuar? S/N")
 
+                                if continuar != "S": # Converte a letra para minuscula 
+                                 print("--------Voltando ao menu da estante--------")
+                                 break
+                                 
+# 6.2 - Estrutura de acesso a estante 2 / Quantidade de livros
 
                     elif menu_estante2 == "2":
-                                print(f"Sua estante possui {len(estante2)}  livros")    
-                                adicionar_ou_nao = input("Deseja adicionar algum livro ou nao ?S/N")   
-                                if adicionar_ou_nao == "S":
+                            print(f"Sua estante possui {len(estante2)} livros ")
+                            while True:
+                                adicionar_livros = input("Deseja adicionar algum livro? S/N")
+
+                                if adicionar_livros == "S":
                                     adicionar_livros = input("Digite o livro que você deseja adicionar: ")
                                     estante2.append(adicionar_livros)
-                                    print("livro adicionado com sucesso!")
+                                    print(f"livro {adicionar_livros} adicionado com sucesso!")
                                 else:
-                                    while True: 
-                                        deseja_continuar2 = input("Escolha a opcao: \nOpcao 1 - Menu da Estante \nOpcao 2 - Menu Principal\nAguardando sua escolha....")
+                                    print("--------Voltando ao menu da estante--------")
+                                    break
 
-                                        if deseja_continuar2 == "1":
-                                            print("Acessando o menu da estante .....")
-                                            menu_estante2 = input("Opção 1 - Adicionar livros \n Opção 2 - Ver a quantidade de livros \n Opção 3 - Voltar ao Menu Principal\nAguardando sua escolha....")
+# 6.3 - Estrutura de acesso a estante 2 / Livros Adicionados
 
-                                        elif deseja_continuar2 == "2":
-                                            print("Retornando ao menu principal...")
-                                            menu_principal = input("- Opção 1 - Estante 1 \n- Opção 2 - Estante 2\n- Opção 3 - Sair do programa\nAguardando sua escolha....")
-
-                                        else:
-                                            print("Retornando ao menu principal....")
-                                            break
-                    else:
-                        print("Retornando ao menu principal....")
-                        break
-
-# 5 . Estrtura de condicao caso o usario queria sair do sistema    
-    else:
-        print("Saindo do programa...")  
-        break   
-       
-
-    
-                           
-    
-
-                                
-                             
-                                    
-                
-                                
-                                
-
-
-                    
+                    elif menu_estante2 == "3":
+                        print(estante2)
+                        print("--------Voltando ao menu da estante--------")
+                        print(f"--------Acessando o menu da estante {menu_principal}--------\n")
+                        menu_estante2 = input(    
+                                            "1 - Adicionar livros\n"
+                                            "2 - Ver quantidade\n"
+                                            "3 - Livros adicionados \n"
+                                            "4 - Voltar ao menu principal\n"
+                                            "Aguardando sua escolha...."
+                                            )
                         
-                                
-
-
-                        
-
-      
         
-                  
-            
-                
 
+# 6.4 - Retorno ao menu principal
 
-
-
-
-
-
-
-
-
+                    elif menu_estante2 == "4":
+                        print("--------Voltando ao menu da principal-------")
+                        break
     
+                         
 
+# 7 - Saida do sistema
+    else:  
+     print("Saindo do programa")
+     break
